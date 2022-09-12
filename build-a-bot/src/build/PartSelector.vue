d03c530e1f151f3f87ece32a8290e5ae2d5f7db2
Rayssa Alves <rayssa.a.vieira@outlook.com>
2022-09-12 16:26:29 +0200

    working with route params

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -1,16 +1,16 @@
 <template>
     <div class="part" :class="position">
-      <img :src="selectedPart.src" @click="showPartInfo= !showPartInfo" title="arm"/>
+      <router-link :to="{
+        name: 'Parts',
+        params: {
+          id: selectedPart.id,
+          partType: selectedPart.type,
+        }}">
+        <img :src="selectedPart.src" title="arm"/>
+      </router-link>
       <button @click="selectPreviousPart()" class="prev-selector"></button>
       <button @click="selectNextPart()" class="next-selector"></button>
       <span class="sale" v-show="selectedPart.onSale">Sale!</span>
-      <teleport to="#partInfo" v-if="showPartInfo">
-        <div>
-          <div>{{selectedPart.cost}} {{selectedPart.title}} {{selectedPart.type}}</div>
-          <div>{{selectedPart.description}}</div>
-          <hr/>
-        </div>
-      </teleport>
     </div>
   </template>
 
@@ -39,7 +39,7 @@
     },
 },
     data() {
-      return { selectedPartIndex: 0, showPartInfo: false };
+      return { selectedPartIndex: 0 };
     },
     computed: {
       selectedPart() {
@@ -99,6 +99,7 @@
   }
   .part img {
     width:165px;
+    cursor: pointer;
   }
   .top {
     border-bottom: none;
--- build-a-bot/src/router/index.js
+++ build-a-bot/src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from 'vue-router';
 
 import HomePage from '../home/HomePage.vue';
 import RobotBuilder from '../build/RobotBuilder.vue';
+import PartInfo from '../parts/PartInfo.vue';
 
 export default createRouter({
 history: createWebHashHistory(),
@@ -13,5 +14,9 @@ component: HomePage,
     path: '/build',
     name: 'Build',
     component: RobotBuilder,
-    }],
+    }, {
+        path: '/parts/:partType/:id',
+        name: 'Parts',
+        component: PartInfo,
+        }],
 });
