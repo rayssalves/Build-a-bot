d22e3687c4469cfcbaa991a7fbe748efc5200839
Rayssa Alves <rayssa.a.vieira@outlook.com>
2022-09-19 15:36:29 +0200

    warning for navigating away

--- build-a-bot/src/build/RobotBuilder.vue
+++ build-a-bot/src/build/RobotBuilder.vue
@@ -75,10 +75,21 @@ import CollapsibleSection from '../shared/CollapsibleSection.vue';
 // all the components go here
 export default {
   name: 'RobotBuilder',
+  beforeRouteLeave(to, from, next) {
+    if (this.addedToCart) {
+      next(true);
+    } else {
+      /* eslint no-alert: 0 */
+      /* eslint no-restricted-globals: 0 */
+      const response = confirm('You have not added your robot to your cart, are you sure you want to leave?');
+      next(response);
+    }
+  },
   components: { PartSelector, CollapsibleSection },
   data() {
     return {
       availableParts,
+      addedToCart: false,
       cart: [],
       selectedRobot: {
         head: {},
@@ -102,6 +113,7 @@ export default {
         + robot.base.cost;
       // eslint-disable-next-line prefer-object-spread
       this.cart.push(Object.assign({}, robot, { cost }));
+      this.addedToCart = true;
       },
     },
   };
