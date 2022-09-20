6f635fb603d5d46152d33d009c16b4179d8ccebd
Rayssa Alves <rayssa.a.vieira@outlook.com>
2022-09-20 14:50:28 +0200

    using vuex getters

--- build-a-bot/src/store/index.js
+++ build-a-bot/src/store/index.js
@@ -10,4 +10,9 @@ export default createStore({
             state.cart.push(robot);
         },
     },
+    getters: {
+        cartSaleItems(state) {
+            return state.cart.filter((item) => item.head.onSale);
+        },
+    },
 });
