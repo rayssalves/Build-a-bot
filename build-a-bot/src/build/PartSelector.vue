dff20db80a0b001938bc2f805834bd8d5e51b27a
Rayssa Alves <rayssa.a.vieira@outlook.com>
2022-09-26 14:10:16 +0200

    using directives and life cycle hooks

--- build-a-bot/src/build/PartSelector.vue
+++ build-a-bot/src/build/PartSelector.vue
@@ -10,11 +10,16 @@
       </router-link>
       <button @click="selectPreviousPart()" class="prev-selector"></button>
       <button @click="selectNextPart()" class="next-selector"></button>
-      <span class="sale" v-show="selectedPart.onSale">Sale!</span>
+      <span
+      @click="pinPadding='30px'"
+       v-pin="{ bottom: pinPadding, right: pinPadding }"
+      class="sale" v-show="selectedPart.onSale">Sale!</span>
     </div>
   </template>
 
   <script>
+  import pinDirective from '../shared/pin-directive';
+
   function getPreviousValidIndex(index, length) {
     const deprecatedIndex = index - 1;
     return deprecatedIndex < 0 ? length - 1 : deprecatedIndex;
@@ -24,6 +29,7 @@
     return incrementedIndex > length - 1 ? 0 : incrementedIndex;
   }
   export default {
+    directives: { pin: pinDirective },
     props:
     {
  parts: {
@@ -39,7 +45,10 @@
     },
 },
     data() {
-      return { selectedPartIndex: 0 };
+      return {
+        selectedPartIndex: 0,
+        pinPadding: '10px',
+      };
     },
     computed: {
       selectedPart() {
@@ -81,9 +90,6 @@
     border: 3px solid #aaa;
   }
   .sale {
-    position: absolute;
-    bottom: 5px;
-    right: 5px;
     color: white;
     background-color: red;
     padding: 3px;
---
+++ build-a-bot/src/shared/pin-directive.js
@@ -0,0 +1,8 @@
+export default function (element, binding) {
+    Object.keys(binding.value).forEach((position) => {
+        // eslint-disable-next-line no-param-reassign
+        element.style[position] = binding.value[position];
+    });
+    // eslint-disable-next-line no-param-reassign
+    element.style.position = 'absolute';
+}
